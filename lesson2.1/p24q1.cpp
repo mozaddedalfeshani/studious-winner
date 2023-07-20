06809841fc7007e488bec8b8ad053b7f5c79ee26
mozaddedalfeshani <mozaddedalfeshanimurad@gmail.com>
2023-07-20 19:34:48 +0600

    nothing

--- lesson2.1/p24q1.cpp
+++ lesson2.1/p24q1.cpp
@@ -2,16 +2,21 @@
 #include<cmath>
 using namespace std;
 int main(){
-    double base, pendicular, hypotenuse;
-    cout<< "Enter the base : "<<endl;
-    cin>> base;
+    double pi = 3.14159265358975;
+    int base, pendicular, hypotenus ;
+    
     cout<< "Enter the pendiculer : "<<endl;
     cin>> pendicular;
+    cout<< "Enter the base : "<<endl;
+    cin>> base;
     cout<< "Enter the hypotenus : "<<endl;
     cin>> hypotenus;
+    
+    double x = atan(3/4);
+    cout<< x;
 
-    cout<< "The angle beetween base and pendicular "<< atan((pendicular/base))<<endl;
-    cout<< "The angle beetween base and hypotenus "<< acos((base/hypotenus))<<endl;
-    cout<< "The angle beetween pendiculer and hypotenus "<< asin((pendicular/hypotenus))<<endl;
+    // cout<< "The angle beetween base and pendicular "<< ((atan((pendicular/base)))*(180/pi))<<endl;
+    // cout<< "The angle beetween base and hypotenus "<< acos((base/hypotenus))<<endl;
+    // cout<< "The angle beetween pendiculer and hypotenus "<< asin((pendicular/hypotenus))<<endl;
     return 0;
 }
